c90cd61467d89a76b9662aeeef05d546fa7f9a7e
Sneha Patil <sneha.patil13sp@gmail.com>
2023-07-27 23:38:41 +0530

    Create hello.js

---
+++ practice/hello.js
@@ -0,0 +1,16 @@
+// regular print
+console.log("Hello World!");
+
+// print variable
+let hello = "Hello World!";
+console.log(hello);
+
+// slicing the string and then print
+let justWorld = hello.slice(6);
+console.log(justWorld);
+
+// print variable value
+console.log(`Printing ${hello}`);
+
+// print using standard output
+process.stdout.write("Hello World!");
